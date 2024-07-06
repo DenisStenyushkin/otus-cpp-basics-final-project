fce2d493cb3daa1ea36728c0bc63355c5b33ba5d
Denis Stenyushkin <denisstenyushkin@yandex.ru>
2024-07-06 18:37:13 +0000

    Devcontainer config

---
+++ .devcontainer/install-dev-tools.sh
@@ -0,0 +1,36 @@
+# update system
+apt-get update
+apt-get upgrade -y
+# install Linux tools and Python 3
+apt-get install software-properties-common wget curl \
+    python3-dev python3-pip python3-wheel python3-setuptools -y
+# install Python packages
+python3 -m pip install --upgrade pip
+pip3 install --user -r .devcontainer/requirements.txt
+# update CUDA Linux GPG repository key
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb
+dpkg -i cuda-keyring_1.0-1_all.deb
+rm cuda-keyring_1.0-1_all.deb
+# install cuDNN
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
+apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub
+add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /" -y
+apt-get update
+apt-get install libcudnn8=8.9.0.*-1+cuda11.8
+apt-get install libcudnn8-dev=8.9.0.*-1+cuda11.8
+# install recommended packages
+apt-get install zlib1g g++ git freeglut3-dev \
+    libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev libfreeimage-dev -y
+# install cmake
+apt-get install -y build-essential libssl-dev
+cd /tmp
+wget https://github.com/Kitware/CMake/releases/download/v3.20.0/cmake-3.20.0.tar.gz
+tar -zxvf cmake-3.20.0.tar.gz
+cd cmake-3.20.0/
+./bootstrap
+make && make install
+# clean up
+pip3 cache purge
+apt-get autoremove -y
+apt-get clean
